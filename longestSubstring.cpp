dd68a38a63766d25fcfd3ae036f646b8602aa8de
Archit <architkeshri4@gmail.com>
2021-03-16 22:09:39 +0530

    longest substring - added testcase

--- longestSubstring.cpp
+++ longestSubstring.cpp
@@ -3,43 +3,48 @@
 
 using namespace std;
 int main(){
-
-    string s;
-    cin >> s;
-    int ans = INT_MIN;
-    int st,end;
-    for (int starting_index = 0; starting_index < s.size(); starting_index++)
-    {
-        for (int array_size = 1; array_size + starting_index < s.size() ; array_size++)
-        {   
-            int i = starting_index;
-            int j = array_size +starting_index;
-            while (i <= j)
-            {
-                if(s[i] == s[j]){
-                    i++;
-                    j--;
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        int ans = INT_MIN;
+        int st=0,end=0;
+        for (int starting_index = 0; starting_index < s.size(); starting_index++)
+        {
+            for (int array_size = 1; array_size + starting_index < s.size() ; array_size++)
+            {   
+                int i = starting_index;
+                int j = array_size +starting_index;
+                while (i <= j)
+                {
+                    if(s[i] == s[j]){
+                        i++;
+                        j--;
+                    }
+                    else{
+                        break;
+                    }
                 }
-                else{
-                    break;
+                if(i > j){
+                    if(array_size > ans){
+                        ans = array_size  ;
+                        st = starting_index;
+                        end = starting_index + array_size;
+                    }
                 }
-            }
-            if(i > j){
-                if(array_size > ans){
-                    ans = array_size  ;
-                    st = starting_index;
-                    end = starting_index + array_size;
-                }
+                
             }
             
         }
-        
-    }
 
-    for (int i = st; i < end+1; i++)
-    {
-        cout << s[i];
+        for (int i = st; i < end+1; i++)
+        {
+            cout << s[i];
+        }
+        cout << endl;
     }
+   
     
     
     return 0;
