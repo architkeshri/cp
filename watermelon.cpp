d5f5f5f278bd98bae87039d11d9bdf0bab0338de
Archit <architkeshri4@gmail.com>
2021-03-20 23:49:23 +0530

    codeforces 4a-watermelon

---
+++ watermelon.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+int main(){
+
+    int n;
+    cin >> n;
+    if(n&1 || n==2){
+        cout << "NO";
+    }
+    else
+        cout << "YES";
+
+    return 0;
+}
