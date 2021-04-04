86a835b4e474cf0ffa18d23062bdf40682e4c0d9
Archit <architkeshri4@gmail.com>
2021-04-04 17:09:55 +0530

    Memoization Lcs Dp

---
+++ lcsMemoization.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+int t[1002][1002];
+
+int solve(string s1, string s2, int s1_size, int s2_size){
+    if(s1_size == 0 || s2_size == 0){
+        
+        return 0;
+    }
+    if(t[s1_size][s2_size] != -1){
+        return t[s1_size][s2_size];
+    }
+    if(s1[s1_size-1] == s2[s2_size-1]){
+        return t[s1_size][s2_size] =  1 + solve(s1, s2, s1_size-1, s2_size-1);
+    }
+    else{
+        return t[s1_size][s2_size] = max(solve(s1, s2, s1_size-1, s2_size), solve(s1, s2, s1_size, s2_size-1));
+    }
+}
+int main(){
+    memset(t, -1, sizeof(t));
+    string s1, s2;
+    cin >> s1 >> s2;
+    cout << solve(s1, s2, s1.size(), s2.size());
+    return 0;
+}
